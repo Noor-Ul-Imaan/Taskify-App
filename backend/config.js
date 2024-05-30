93353e0708983afb2d854e964865ab93d4ff1f93
alishba29 <khaweralishba@gmail.com>
2024-05-31 03:13:43 +0500

    instead of

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5000;
 
 //export const mongoDBURL = 'mongodb+srv://Admin123:<Admin123@>@taskify.rcgvhyp.mongodb.net/?retryWrites=true&w=majority&appName=TASKIFY'
-export const mongoDBURL = 'mongodb+srv://Admin123:Admin123%40@taskify.rcgvhyp.mongodb.net/?retryWrites=true&w=majority&appName=TASKIFY';
-
+export const mongoDBURL =
+  "mongodb+srv://Admin123:Admin123%40@taskify.rcgvhyp.mongodb.net/?retryWrites=true&w=majority&appName=TASKIFY";
