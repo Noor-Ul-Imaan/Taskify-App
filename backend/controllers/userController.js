585c2980830415313b64a7a9ff2f67bb7269288b
alishba29 <khaweralishba@gmail.com>
2024-05-31 03:10:45 +0500

    comment login/signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,32 @@
-import User from '../models/userModel.js';
-import { signup } from '../models/userModel.js';
-import jwt from 'jsonwebtoken'
+// import User from '../models/userModel.js';
+//import { signup } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-  }
-
+  // return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login a user
 export const loginUser = async (req, res) => {
-    const {email, password} = req.body
-
-    try {
-      const user = await User.login(email, password)
-  
-      // create a token
-      const token = createToken(user._id)
-  
-      res.status(200).json({email, token})
-    } catch (error) {
-      res.status(400).json({error: error.message})
-    }
+  // const { email, password } = req.body;
+  // try {
+  //   const user = await User.login(email, password);
+  //   // create a token
+  //   const token = createToken(user._id);
+  //   res.status(200).json({ email, token });
+  // } catch (error) {
+  //   res.status(400).json({ error: error.message });
+  // }
 };
 
 // signup a user
 export const signupUser = async (req, res) => {
-    const {name, email, password} = req.body
-
-    try {
-      const user = await User.signup(name, email, password)
-      const token = createToken(user._id)
-
-      res.status(200).json({name: user.name, email: user.email, token})
-    } catch (error) {
-      res.status(400).json({error: error.message})
-    }
+  // const { name, email, password } = req.body;
+  // try {
+  //   const user = await User.signup(name, email, password);
+  //   const token = createToken(user._id);
+  //   res.status(200).json({ name: user.name, email: user.email, token });
+  // } catch (error) {
+  //   res.status(400).json({ error: error.message });
+  // }
 };
