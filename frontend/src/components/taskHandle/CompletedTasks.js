6b26ffd10f191f207d8feb583dfc0a835281aefb
aamnakayy <khalid4430146@cloud.neduet.edu.pk>
2024-06-11 01:43:10 +0500

    added rating in completed tasks

--- frontend/src/components/taskHandle/CompletedTasks.js
+++ frontend/src/components/taskHandle/CompletedTasks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import TaskList from './TaskList';
 import './ViewAssigned.css'; // Import the CSS file here
 
 const CompletedTasks = () => {
@@ -22,17 +22,42 @@ const CompletedTasks = () => {
     .catch(error => console.error('Error fetching tasks:', error));
   }, [token, user.username]);
 
+  const handleDeleteTask = (taskId) => {
+    setTasks(tasks.filter(task => task._id !== taskId));
+  };
+
   const handleUpdateTask = (updatedTask) => {
     setTasks(tasks.map(task => task._id === updatedTask._id ? updatedTask : task));
+  };
+
+  const handleDownload = (task) => { 
+    if (task.attachment) {
+      const link = document.createElement('a');
+      link.href = `http://localhost:5000/${task.attachment}`;
+      link.download = task.attachment;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      console.error('Attachment not found for task:', task._id);
+    }
   };
 
   return (
     <div className="container" id="view-tasks-container">
       <h1>Completed Tasks</h1>
-      <TaskList 
-        tasks={tasks} 
-        onUpdateTask={handleUpdateTask}
-      />
+      <div>
+        {tasks.map(task => (
+          <div key={task._id} className="task-section">
+            <h2 className="task-title">{task.title}</h2>
+            <p className="task-details">{task.description}</p>
+            <p className="task-details">Assigned by: {task.assignedBy}</p>
+            <p className="task-details">Deadline: {new Date(task.deadline).toLocaleDateString()}</p>
+            {task.rating && <p className="task-details">Rating:  {task.rating}/5</p>}
+            
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
